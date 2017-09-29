cf6d951e943c0ab669183624371f730999cb9de4
NullPointer <kkjiankang@126.com>
2017-09-29 10:37:52 +0800

    基础框架实现

---
+++ chat/src/main/java/com/drive/service/domain/request/Request.java
@@ -0,0 +1,8 @@
+package com.drive.service.domain.request;
+
+/**
+ * @author
+ * @create 2017-09-21 11:05
+ **/
+public class Request {
+}
---
+++ chat/src/main/java/com/drive/service/domain/response/Response.java
@@ -0,0 +1,8 @@
+package com.drive.service.domain.response;
+
+/**
+ * @author
+ * @create 2017-09-21 10:32
+ **/
+public class Response {
+}
